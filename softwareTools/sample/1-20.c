134af9cbbc71917299dd763972bc89f60e5aafd5
pito <pitohou@hotmail.co.jp>
2016-07-10 21:54:29 +0900

    [プログラミング言語C ANSI]入力されたタブを次のタブストップまでスペースで埋める関数の実装

---
+++ softwareTools/sample/1-20.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAXLINE 100
+#define TABSTOP 4
+
+void detab (char input[], char output[]);
+
+int main () {
+    char input[MAXLINE],output[MAXLINE];
+    if (fgets(input,sizeof input,stdin) != NULL) {
+        detab(input,output);
+        printf("%s\n",output);
+    }
+}
+void detab (char input[], char output[]) {
+    int inputNum,outputNum;
+    inputNum = outputNum = 0;
+    while (input[inputNum] != '\0') {
+        if (input[inputNum] == '\t') {
+            int blankNum = TABSTOP - (inputNum % TABSTOP);
+            for (;blankNum > 0;blankNum--) {
+                output[outputNum++] = 'b';
+            }
+        } else {
+            output[outputNum++] = input[inputNum];
+        }
+        inputNum++;
+    }
+    output[outputNum] = '\0';
+}
